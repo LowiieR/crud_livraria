9100fef87e0cb984f99af8a67b1f3cd1d3e79319
LowiieR <louie_ripper@estudante.sesisenai.org.br>
2026-08-13 08:07:44 -0300

    add - revisei e corrigi o código de cadastro com Prepared Statements

--- public/cadastrar.php
+++ public/cadastrar.php
@@ -6,9 +6,19 @@ $titulo = $_POST["titulo"];
 $autor = $_POST["autor"];
 $ano = $_POST["ano"];
 
-$sql = "INSERT INTO livros (titulo,autor,ano) VALUES ('$titulo','$autor','$ano')";
+if (empty($titulo) || empty($autor) || empty($ano)) {
+    die("Todos os campos devem ser preenchidos.");
+}
 
-mysqli_query($conexao, $sql);
+$sql = "INSERT INTO livros (titulo,autor,ano) VALUES ('?','?','?')";
+
+$stmt = mysqli_prepare($conexao, $sql);
+
+mysqli_stmt_bind_param($stmt, "ssi", $titulo, $autor, $ano);
+
+mysqli_stmt_execute($stmt);
+
+mysqli_stmt_close($stmt);
 
 header("Location: ../index.php");
 ?>
